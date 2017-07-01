e9206ba761f6532fd60cab387acb364173949fd1
Zack <Zack@192.168.0.106>
2017-06-30 21:30:56 -0500

    Updated ExampleMod and added new packages

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -13,10 +13,22 @@ public class ExampleMod
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
     
-    @EventHandler
+    @Mod.EventHandler
+    public void preInit(FMLInitializationEvent event)
+    {
+    	
+    }
+    
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+    
+    @Mod.EventHandler
+    public void postInit(FMLInitializationEvent event)
+    {
+    	
     }
 }
