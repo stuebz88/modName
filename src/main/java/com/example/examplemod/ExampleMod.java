8f50b7e8fdd094da6ec5bd866ece82746a990ac1
christopherstockus <christopherstockus@141.233.65.2>
2017-06-30 20:15:01 -0500

    Test with conflicting info.  I'm not a fattie

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,7 +1,7 @@
 package com.example.examplemod;
 
 // Test Comment
-// hi test hello world whatevs
+// I'm not fat
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
