5bc0f7df6f35d42a0c7f15a96f9812dd81f9bf1f
Zack Stuebs <stuebz88@uwosh.edu>
2017-06-30 22:29:38 -0500

    Sorting out ModBlocks issue

--- src/main/java/com/example/examplemod/common/blocks/ModBlocks.java
+++ src/main/java/com/example/examplemod/common/blocks/ModBlocks.java
@@ -1,10 +1,15 @@
 package com.example.examplemod.common.blocks;
 
+import com.jcraft.jorbis.Block;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import scala.tools.nsc.backend.icode.BasicBlocks.BasicBlock;
+
 public final class ModBlocks {
 
 	public static Block modBlock;
 	
 	public static void createBlocks() {
-		GameRegistry.registerBlock(modBlock = new BasicBlock("modBlock").setLightLevel(1.0f), "modBlock")
+		//GameRegistry.registerBlock(modBlock = new BasicBlock("modBlock").setLightLevel(1.0f), "modBlock")
 	}
 }
