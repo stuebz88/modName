81b58ca4eca75fd3f1a641007f28be05d3b5ed60
christopherstockus <christopherstockus@141.233.65.2>
2017-06-30 20:09:11 -0500

    Added a descriptive and helpful test comment </descriptive and helpful>

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,6 +1,7 @@
 package com.example.examplemod;
 
 // Test Comment
+// hi test hello world whatever
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
