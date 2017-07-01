f4f876384a9f6a9adea59454edb0d65f1a4a9133
powerlinedaydreams <christopher.stockus@gmail.com>
2017-06-30 22:24:09 -0500

    Imported references in ModBlocks.java

--- src/main/java/com/example/examplemod/common/blocks/ModBlocks.java
+++ src/main/java/com/example/examplemod/common/blocks/ModBlocks.java
@@ -1,5 +1,10 @@
 package com.example.examplemod.common.blocks;
 
+import com.jcraft.jorbis.Block;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import scala.tools.nsc.backend.icode.BasicBlocks.BasicBlock;
+
 public final class ModBlocks {
 
 	public static Block modBlock;
