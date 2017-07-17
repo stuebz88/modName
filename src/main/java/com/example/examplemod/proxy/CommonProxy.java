f99868da9203bbaeee5a9f78bca16b7f21fdbe9b
powerlinedaydreams <christopher.stockus@gmail.com>
2017-07-17 13:44:23 -0500

    Committing CommonProxy to resolve pull issues

--- src/main/java/com/example/examplemod/proxy/CommonProxy.java
+++ src/main/java/com/example/examplemod/proxy/CommonProxy.java
@@ -1,8 +1,14 @@
 package com.example.examplemod.proxy;
 
+import com.example.examplemod.common.core.EventHandlerCommon;
+import com.example.examplemod.common.core.ITurn;
+import com.example.examplemod.common.core.WorldTurn;
+import com.example.examplemod.common.core.WorldTurnStorage;
 import com.example.examplemod.init.ModRegistry;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -17,14 +23,17 @@ public class CommonProxy
 		config = new Configuration(new File(configDir.getPath(), "examplemod.cfg"));
 		Config.readConfig();*/
 		MinecraftForge.EVENT_BUS.register(new ModRegistry());
+		CapabilityManager.INSTANCE.register(ITurn.class, new WorldTurnStorage(), WorldTurn.class);
+		MinecraftForge.EVENT_BUS.register(new EventHandlerCommon());
 	}
 	
 	public void init(FMLInitializationEvent e)
 	{
+		
 	}
 	
 	public void postInit(FMLPostInitializationEvent e)
 	{
-
+		
 	}
 }
