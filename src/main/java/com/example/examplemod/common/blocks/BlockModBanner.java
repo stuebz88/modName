3770d6cfe003d3390ce72c03d18e99ce56450625
Zack Stuebs <stuebz88@uwosh.edu>
2017-07-25 21:47:19 -0500

    Moved block place event to banner class and started claiming process

--- src/main/java/com/example/examplemod/common/blocks/BlockModBanner.java
+++ src/main/java/com/example/examplemod/common/blocks/BlockModBanner.java
@@ -1,11 +1,19 @@
 package com.example.examplemod.common.blocks;
 
 import com.example.examplemod.Ref;
+import com.example.examplemod.common.map.tile.Tile;
+import com.example.examplemod.common.map.tile.TileList;
+import com.example.examplemod.common.map.tile.TilePos;
 import com.example.examplemod.util.IHasModel;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockModBanner extends Block implements IHasModel
 {
@@ -20,4 +28,11 @@ public class BlockModBanner extends Block implements IHasModel
 		this.setCreativeTab(Ref.TAB);
 		Ref.BLOCKS.add(this);
 	}
+	
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack)
+    {
+		TileList tileList = TileList.get(worldIn);
+		Tile placedOn = tileList.getTileByPos(new TilePos(pos));
+    }
 }
